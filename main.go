df5b0644f7f100d64d5f3e21ed6bf264a7927e2d
Alex Greenbank <alex@greenbank.org>
2022-09-14 07:15:36 +0000

    Shoddily handle disconnected state, needs lots of work!

--- main.go
+++ main.go
@@ -112,7 +112,6 @@ func parseXML(body string, polltime time.Time, dur_poll time.Duration, statuscod
 		return nil
 	}
 	// Find <link_status>
-	// <link_status value="connected%3Bvdsl%3B462385"/>
 	if val := xmlquery.FindOne(x_status, "//link_status/@value"); val != nil {
 		// fmt.Printf("Got raw link_status value of [%s]\n", val.InnerText())
 		str, err := url.QueryUnescape(val.InnerText())
@@ -123,6 +122,7 @@ func parseXML(body string, polltime time.Time, dur_poll time.Duration, statuscod
 		str = strings.Replace(str, "\n", "", -1)
 		// fmt.Printf("Got link_status value of [%s]\n", str)
 		if strings.HasPrefix(str, "connected;") {
+			// <link_status value="connected%3Bvdsl%3B462385"/>
 			n, err := fmt.Sscanf(str, "connected;vdsl;%d", &c_uptime)
 			if err != nil {
 				// TODO - failed to convert uptime
@@ -133,9 +133,24 @@ func parseXML(body string, polltime time.Time, dur_poll time.Duration, statuscod
 				log.Fatal("Failed to convert conn_uptime:2:", err)
 			}
 			// fmt.Printf("Converted conn_uptime of %d\n", c_uptime)
+		} else if strings.HasPrefix(str, "disconnected;") {
+			// <link_status value="disconnected%3Badsl%3B0" />
+			mu.Lock()
+			state = S_DISCONNECTED
+			connuptime = 0
+			lastpoll = polltime
+			laststatuscode = statuscode
+			lastpolldur = dur_poll
+			lastparsedur = dur_parse
+			if err != nil {
+				lasterror = err.Error()
+			}
+			lasterrortime = parsetime
+			mu.Unlock()
+			return nil
 		} else { // TODO handle more states
 			// TODO - handle this more gracefully
-			log.Fatal("Unexpected link_status value:", val)
+			log.Print("Unexpected link_status value:", val)
 		}
 	} else {
 		// TODO - can't get link_status
@@ -270,6 +285,45 @@ func metricText() string {
 	response_text := ""
 	if curr_state == S_INIT {
 		response_text = "Argle\n"
+	} else if curr_state == S_DISCONNECTED {
+		response_text += "# HELP btsmarthub2_connected Whether the Broadband is connected\n"
+		response_text += "# TYPE btsmarthub2_connected gauge\n"
+		response_text += "btsmarthub2_connected 0\n"
+
+		response_text += "# HELP btsmarthub2_laststatuscode The HTTP status code of the last poll\n"
+		response_text += "# TYPE btsmarthub2_laststatuscode gauge\n"
+		response_text += "btsmarthub2_laststatuscode " + str_laststatuscode + "\n"
+
+		response_text += "# HELP btsmarthub2_lastgoodpoll_seconds The UNIX timestamp in seconds of the last good poll\n"
+		response_text += "# TYPE btsmarthub2_lastgoodpoll_seconds counter\n"
+		response_text += "btsmarthub2_lastgoodpoll_seconds " + str_lastgoodpoll + "\n"
+
+		response_text += "# HELP btsmarthub2_lastpoll_seconds The UNIX timestamp in seconds of the last poll\n"
+		response_text += "# TYPE btsmarthub2_lastpoll_seconds counter\n"
+		response_text += "btsmarthub2_lastpoll_seconds " + str_lastpoll + "\n"
+
+		response_text += "# HELP btsmarthub2_lastpolldur_seconds The duration of the poll\n"
+		response_text += "# TYPE btsmarthub2_lastpolldur_seconds counter\n"
+		response_text += "btsmarthub2_lastpolldur_seconds " + str_lastpolldur + "\n"
+
+		response_text += "# HELP btsmarthub2_lastparsedur_seconds The duration of the poll\n"
+		response_text += "# TYPE btsmarthub2_lastparsedur_seconds counter\n"
+		response_text += "btsmarthub2_lastparsedur_seconds " + str_lastparsedur + "\n"
+
+		if str_lasterror != "" {
+			response_text += "# HELP btsmarthub2_lasterrortime_seconds The UNIX timestamp in seconds of the last error\n"
+			response_text += "# TYPE btsmarthub2_lasterrortime_seconds counter\n"
+			response_text += "btsmarthub2_lasterrortime_seconds " + str_lasterrortime + "\n"
+		}
+
+		response_text += "# HELP btsmarthub2_sysuptime_seconds The sysuptime of the BB router in seconds\n"
+		response_text += "# TYPE btsmarthub2_sysuptime_seconds counter\n"
+		response_text += "btsmarthub2_sysuptime_seconds " + str_sysuptime + "\n"
+
+		response_text += "# HELP btsmarthub2_connuptime_seconds The connuptime of the BB connection in seconds\n"
+		response_text += "# TYPE btsmarthub2_connuptime_seconds counter\n"
+		response_text += "btsmarthub2_connuptime_seconds " + str_connuptime + "\n"
+
 	} else if curr_state == S_CONNECTED {
 		response_text += "# HELP btsmarthub2_connected Whether the Broadband is connected\n"
 		response_text += "# TYPE btsmarthub2_connected gauge\n"
@@ -401,8 +455,8 @@ func main() {
 			log.Fatal("parseXML:", err)
 		}
 		// output data
-		//output := metricText()
-		//fmt.Printf("%s", output)
+		// output := metricText()
+		// fmt.Printf("%s", output)
 		return
 	}
 
